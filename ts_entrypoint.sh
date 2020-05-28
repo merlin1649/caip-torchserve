7810d7cd4b40c0e217283609be82c9c98606c49f
Kevin Tsai <kevin0259@gmail.com>
2020-05-28 01:03:55 -0700

    updated model extension

--- ts_entrypoint.sh
+++ ts_entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-torchserve --start  --ncs --model-store ${AIP_STORAGE_URI} --models ${AIP_MODEL_NAME}
+torchserve --start  --ncs --model-store ${AIP_STORAGE_URI} --models ${AIP_MODEL_NAME}.mar
